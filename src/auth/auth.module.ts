dbb6d1ffea92e93e0fa63f4cbbb3d2250e47ad4d
b-arvo <brennanarvo@gmail.com>
2020-01-11 12:43:43 -0500

    Get user object from Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [GoogleStrategy],
 })
 export class AuthModule {}
